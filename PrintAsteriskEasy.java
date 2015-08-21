41ebca112442809ce49802141725f6c99bdb5fa4
anshup <anshup@INanshup.local>
2015-08-22 02:47:49 +0530

    Question 1

---
+++ PrintAsteriskEasy.java
@@ -0,0 +1,10 @@
+/**
+ * Created by anshup on 20/08/15.
+ */
+public class PrintAsteriskEasy {
+    //Print one asterisk to the console.
+    public static void main(String args[]){
+
+        System.out.print("*");
+    }
+}
